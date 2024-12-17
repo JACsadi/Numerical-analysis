577adb3ff84886478f527643a573db4034d80e8f
2021331096 <2021331096@student.sust.edu>
2024-12-17 15:58:50 +0600

    h

---
+++ findingRoot.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+//ax^3 + bx^2 + c^x + d = 0
+double bs(int a, int b, int c, int d) {
+   double l = 0 , r = 100;
+   int t= 400;
+   while(t--) {
+    //x^3 - 4x - 9
+     double m = (l+r)/2.0;
+     double ans = (a*(m*m*m)) + (b * (m*m)) + (c*m) + d;
+     double ansr = (a*((r)*(r)*(r))) + (b * ((r)*(r))) + (c*(r)) + d;
+     double ansl =  (a*((l)*(l)*(l))) + (b * ((l)*(l))) + (c*(l)) + d;
+     double err = (ans - ansr)/ans;
+     cout << "l == " << l << " r == " << r << " x == " << m << " f(x) == " << ans << " error == " << err << endl ;
+     if(ans == 0) break;
+     else if(ansr==0) l = m;
+     else if(ansl == 0) r=m; 
+     if( ans*ansr <= 0) {
+         l = m;
+        //  r -= 1e-8;
+     } else {r = m; } ;
+   }
+   return 1.0;
+}
+int main() {
+   cout << bs(1.0,0.0,-4.0,-9.0);
+}
---
+++ regulafalsi.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ double a,b,c,d;
+ double guess1 , guess2;
+double f(double x) {
+    return ((a*x*x*x) + (b*x*x) + c*x + d);
+    //x^3 - 4x -9;
+}
+void regulafalsi() {
+    double tolerance = 1e-17; int maxIterations = 10000;
+    double x1=guess1,x2=guess2;
+    double x3 = x1;
+    int iteration = 0;
+    while (iteration < maxIterations) {
+        x3 = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1));
+        printf("x--%.15lf f(x) -- %.15lf \n",x3,f(x3));
+        if (fabs(f(x3)) < tolerance) {
+            return;
+        }
+        if (f(x1) * f(x3) < 0) {
+            x2 = x3; 
+        } else {
+            x1 = x3; 
+        }
+        iteration++;
+    }
+    
+}
+    
+
+int main() {
+    cin >>a>>b>>c>>d;
+    cin >> guess1 >> guess2;
+    regulafalsi();
+}
